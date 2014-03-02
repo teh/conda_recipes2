af785aae5183da941e1ea64eaba3287cddc5a03d
Thomas Hunger <tehunger@gmail.com>
2014-03-02 17:40:21 +0000

    Build CVC4 from master + enable swig.

--- cvc4/build.sh
+++ cvc4/build.sh
@@ -1,4 +1,4 @@
-./configure --prefix=${PREFIX} --without-readline
+./autogen.sh
+./configure --prefix=${PREFIX} --without-readline --with-swig=swig2.0 --enable-language-bindings=python
 make
 make install
-
